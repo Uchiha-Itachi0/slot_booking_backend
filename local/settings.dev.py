50668f7fdd3824d43bfeb213c58cf2ef3808d211
Anubhav Shukla <anubhav008shukla@gmail.com>
2025-01-19 21:59:25 +0530

    Added loggin file for python

--- local/settings.dev.py
+++ local/settings.dev.py
@@ -3,3 +3,11 @@ DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-ba*+g4%c!c0qsy3_4m4w0hepzek=n&ah!^u9fo83zi*80y63^l'
+
+LOGGING['formatters']['colored'] = {  # type: ignore # noqa: F821
+    '()': 'colorlog.ColoredFormatter',
+    'format': '%(log_color)s%(asctime)s %(levelname)s %(name)s %(bold_white)s%(message)s',
+}
+LOGGING['loggers']['slot_booking_backend']['level'] = 'DEBUG'  # type: ignore # noqa: F821
+LOGGING['handlers']['console']['level'] = 'DEBUG'  # type: ignore # noqa: F821
+LOGGING['handlers']['console']['formatter'] = 'colored'  # type: ignore # noqa: F821
